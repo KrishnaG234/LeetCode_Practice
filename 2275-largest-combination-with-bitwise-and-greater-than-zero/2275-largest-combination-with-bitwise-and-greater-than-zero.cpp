1c2163abd8c97fe36a67dce566da5eb8d873c474
Krishna Garg <gargkrishna828@gmail.com>
2024-11-07 17:29:34 +0530

    Time: 35 ms (43.28%), Space: 60.3 MB (32.98%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int largestCombination(vector<int>& candidates) {
-        int maxCount = 0; 
+        int maxCount = 0;
         for (int i = 0; i < 24; i++) {
-            int count = 0;  
+            int count = 0;
             for (int num : candidates) {
-                if ((num & (1 << i)) != 0) {  // Check if the i-th bit is set.
+                if ((num & (1 << i)) != 0) {
                     count++;
                 }
             }
-            maxCount = max(maxCount, count);  // Update the maximum count.
+            maxCount = max(maxCount, count);
         }
         return maxCount;
     }
